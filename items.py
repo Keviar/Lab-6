ddb1da79abcc3bb6db6fc386f8e2983694911be2
Sam Davis <samdavis4@hotmail.co.uk>
2017-10-18 16:18:07 +0100

    Initial commit

---
+++ items.py
@@ -0,0 +1,63 @@
+item_id = {
+    "id": "id",
+
+    "name": "id card",
+
+    "mass": 300,
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?..."""
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "name": "laptop",
+
+    "mass": 2999,
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!"
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "money",
+
+    "mass": 800,
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees."
+}
+
+item_biscuits = {
+    "id": "biscuits",
+
+    "name": "a pack of biscuits",
+
+    "mass": 1200,
+
+    "description": "A pack of biscuits."
+}
+
+item_pen = {
+    "id": "pen",
+    
+    "name": "a pen",
+
+    "mass": 200,
+
+    "description": "A basic ballpoint pen."
+}
+
+item_handbook = {
+    "id": "handbook",
+    
+    "name": "a student handbook",
+
+    "mass": 800,
+
+    "description": "This student handbook explains everything. Seriously."
+}
